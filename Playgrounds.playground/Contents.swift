212a774e23285b90de2ffaa6f09d8b26c251d372
JoyceMatos <j.matosjoyce@gmail.com>
2016-08-29 16:41:09 -0400

    Completed

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,18 @@
-let str = "Hello, world"
+//: Playground - noun: a place where people can play
 
+
+var favoriteWord = "Solidify"
+var favoriteColor = "Red"
+var favoriteMusician = "Amy Winehouse"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("I'm printing on Swift!")
+
+let color = "Blue"
+print("My favorite color is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+var skySentence = "The sky is \(color)."
+print(skySentence)
